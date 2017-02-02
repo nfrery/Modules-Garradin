1aff5934bba7fa5d2a9be47132e52db95fb5c185
Nicolas Frery <nicolas@zoubi.info>
2017-02-02 22:14:35 +0100

    Premier jet pour le module de reçu

--- plugin-recu-don/www/admin/index.php
+++ plugin-recu-don/www/admin/index.php
@@ -9,15 +9,5 @@ if ($user['droits']['config'] < Membres::DROIT_ADMIN)
     throw new UserException("Vous n'avez pas le droit d'accéder à cette page.");
 }
 
-/**
-*	Récupération des infos sur l'association
-*/
-//$GenDon = new Plugin\GenDon;
-//$informations = $GenDon->getInfo();
-
-//$pdf = GenCon::GenPDF();
-//GenCon::Output($pdf);
-$machine = "bidule";
-$tpl->assign('machin', $machine);
 
 $tpl->display(PLUGIN_ROOT . '/templates/index.tpl');
